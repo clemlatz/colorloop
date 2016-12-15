7ae7dd44c846b2bd538124e56ce2f9ac7a233264
Clement Bourgoin <cb@nokto.net>
2016-12-15 21:38:14 +0100

    Look for bridges through upnp if host is undefined

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const hue = require("node-hue-api");
+const nconf = require('nconf');
+const hue   = require("node-hue-api");
+
+// Load configurationa
+nconf.argv()
+   .env()
+   .file({ file: 'config.json' });
+
+let host = nconf.get('host');
+
+// If host is undefined, look for the 1st bridge and get IP address
+if (typeof host === 'undefined') {
+  hue.nupnpSearch().then(function(bridges) {
+    host = bridges[0].ipaddress;
+  }).done();
+}
