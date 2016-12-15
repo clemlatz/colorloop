dd405bc775ff66a839b99c56eaa39865fac22e78
Clement Bourgoin <cb@nokto.net>
2016-12-15 21:47:27 +0100

    Handle case when there is no Hue bridges on local network

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const nconf = require('nconf');
 const hue   = require("node-hue-api");
 
-// Load configurationa
-nconf.argv()
-   .env()
-   .file({ file: 'config.json' });
+var HueApi = new hue.HueApi();
 
-let host = nconf.get('host');
+// Load configuration
+nconf.argv().env().file({ file: 'config.json' });
 
 // If host is undefined, look for the 1st bridge and get IP address
+let host = nconf.get('host');
 if (typeof host === 'undefined') {
   hue.nupnpSearch().then(function(bridges) {
+
+    if (bridges.length === 0) {
+      throw "Cannot find any Hue bridges on your local network.";
+    }
+
     host = bridges[0].ipaddress;
+    registerUser(host);
   }).done();
+} else {
+  registerUser(host);
 }
